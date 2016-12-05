3317e8e9e64d650a610819b7f701f7712f0943f8
Liazidi Amine <liazidiiamine@gmail.com>
2016-12-05 11:51:42 +0100

    update

---
+++ map.js
@@ -0,0 +1,35 @@
+$(function(){
+var directionsDisplay = new google.maps.DirectionsRenderer({ draggable: true });
+var directionsService = new google.maps.DirectionsService();
+var map;
+
+$(window).load(function() {
+    var myOptions = {
+        zoom: 10,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        center: new google.maps.LatLng(35.270, -80.837)
+    };
+    map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+    directionsDisplay.setMap(map);
+    directionsDisplay.setPanel(document.getElementById("directions"));
+    
+    $("#routeMode").on("change", function() { calcRoute(); });
+    $("#routeGo").on("click", function() { calcRoute(); });
+    $("#routeClear").on("click", function() { directionsDisplay.setDirections({ routes: [] }); });
+    
+});
+
+
+function calcRoute() {
+    var request = {
+        origin: $("#routeTo").val(),
+        destination: $("#routeFrom").val(),
+        travelMode: google.maps.TravelMode[$("#routeMode").val()]
+    };
+    directionsService.route(request, function(response, status) {
+        if (status == google.maps.DirectionsStatus.OK) {
+            directionsDisplay.setDirections(response);
+        }
+    });
+}
+});
